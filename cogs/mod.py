42a45d088aea43f488197260c2351fb4dafedd25
Kishor <kishorramanan5@gmail.com>
2023-06-27 10:54:01 +0530

    added warn unwarn link_perm commands

--- cogs/mod.py
+++ cogs/mod.py
@@ -16,9 +16,7 @@ class Mod(commands.Cog):
     async def set_prefix(self, ctx, prefix):
         db = self.bot.cluster["guilds"]
         collections = db["custom_prefix"]
-        print(collections.find_one({"_id": ctx.guild.id}))
         if res := collections.find_one({"_id": ctx.guild.id}):
-            print(res["prefix"])
             if res["prefix"] == prefix:
                 await ctx.send("That's already the prefix!")
                 return
@@ -227,7 +225,9 @@ class Mod(commands.Cog):
     @commands.has_permissions(ban_members=True)
     async def unban(self, ctx, member: discord.User, *, reason: str = None):
         try:
-            await ctx.send(embed=await self._unban(ctx.author, ctx.guild, member, reason))
+            await ctx.send(
+                embed=await self._unban(ctx.author, ctx.guild, member, reason)
+            )
         except discord.NotFound:
             embed = discord.Embed(
                 title="Unban Failed",
@@ -259,7 +259,9 @@ class Mod(commands.Cog):
             colour=discord.Colour.red(),
             timestamp=datetime.now(),
         )
-        if isinstance(error, discord.NotFound) or isinstance(error, app_commands.CommandInvokeError):
+        if isinstance(error, discord.NotFound) or isinstance(
+            error, app_commands.CommandInvokeError
+        ):
             embed.description = "The member you tried to unban is not banned!"
         elif isinstance(error, app_commands.errors.MissingPermissions):
             embed.description = (
@@ -276,6 +278,294 @@ class Mod(commands.Cog):
                 session=self.bot.web_client,
                 error=error,
                 content="Unban Slash Command Error",
+                author=interaction.user,
+                guild=interaction.guild,
+                channel=interaction.channel,
+                username="Unknown Error || BlankBot",
+            )
+        await interaction.response.send_message(embed=embed)
+
+    async def _warn(self, ctx, member: discord.Member, reason: str = None):
+        mod = ctx.user if isinstance(ctx, discord.Interaction) else ctx.author
+        db = self.bot.cluster["guilds"]
+        collection = db["warns"]
+        embed = discord.Embed(
+            colour=discord.Colour.green(),
+            timestamp=datetime.now(),
+        )
+        if reason:
+            if not collection.find_one({"_id": member.id}):
+                collection.insert_one(
+                    {
+                        "_id": member.id,
+                        "warns": [
+                            {
+                                "reason": reason,
+                                "moderator": mod.id,
+                            }
+                        ],
+                    }
+                )
+            else:
+                collection.update_one(
+                    {"_id": member.id},
+                    {
+                        "$push": {
+                            "warns": {
+                                "reason": reason,
+                                "moderator": mod.id,
+                            }
+                        }
+                    },
+                )
+            embed.title = "Warning"
+            embed.description = reason
+            embed.add_field(name="Moderator", value=mod.mention)
+            embed.set_footer(
+                text=f"Requested by {mod}",
+                icon_url=mod.avatar.url,
+            )
+        else:
+            embed.title = "Warning Records"
+            data = collection.find_one({"_id": member.id})
+            if data:
+                embed.description = f"All previous warnings for {member.mention}!"
+                for num, warn in enumerate(data["warns"]):
+                    moderator = ctx.guild.get_member(warn["moderator"])
+                    embed.add_field(
+                        name=f"ID: {num+1} || Mod: {moderator}",
+                        value=warn["reason"],
+                        inline=False,
+                    )
+            else:
+                embed.description = f"No warnings found for {member.mention}!"
+        return embed
+
+    @commands.command(name="warn", aliases=["warns"])
+    @commands.has_permissions(manage_messages=True)
+    async def warn(self, ctx, member: discord.Member, *, reason: str = None):
+        if reason:
+            await ctx.send(member.mention, embed=await self._warn(ctx, member, reason))
+        else:
+            await ctx.send(embed=await self._warn(ctx, member))
+
+    @app_commands.command(name="warn", description="Warn a member")
+    @app_commands.checks.has_permissions(manage_messages=True)
+    @app_commands.describe(member="The member to warn")
+    @app_commands.describe(reason="The reason for warning the member")
+    async def warn_slash(
+        self,
+        interaction: discord.Interaction,
+        member: discord.Member,
+        *,
+        reason: str = None,
+    ):
+        if reason:
+            await interaction.response.send_message(
+                member.mention, embed=await self._warn(interaction, member, reason)
+            )
+        else:
+            await interaction.response.send_message(
+                embed=await self._warn(interaction, member)
+            )
+
+    @warn_slash.error
+    async def warn_slash_error(self, interaction, error):
+        embed = discord.Embed(
+            title="Warn Failed",
+            colour=discord.Colour.red(),
+            timestamp=datetime.now(),
+        )
+        if isinstance(error, app_commands.errors.MissingPermissions):
+            embed.description = (
+                "You don't have the required permissions to warn members!"
+            )
+        elif isinstance(error, app_commands.errors.BotMissingPermissions):
+            embed.description = "I don't have the required permissions to warn members!"
+        else:
+            embed.description = "Unknown error!"
+            await report_error(
+                error_webhook_url=self.bot.unknown_error_webhook_url,
+                session=self.bot.web_client,
+                error=error,
+                content="Warn Slash Command Error",
+                author=interaction.user,
+                guild=interaction.guild,
+                channel=interaction.channel,
+                username="Unknown Error || BlankBot",
+            )
+        await interaction.response.send_message(embed=embed)
+
+    async def _unwarn(self, member: discord.Member, index: int = None):
+        db = self.bot.cluster["guilds"]
+        collection = db["warns"]
+        embed = discord.Embed(
+            colour=discord.Colour.green(),
+            timestamp=datetime.now(),
+        )
+        if data := collection.find_one({"_id": member.id}):
+            if index:
+                try:
+                    popped = data["warns"].pop(index)
+                    collection.update_one(
+                        {"_id": member.id},
+                        {"$set": {"warns": data["warns"]}},
+                    )
+                    embed.description = (
+                        f"Unwarned {member.mention} `{popped['reason']}`!"
+                    )
+                except IndexError:
+                    embed.description = (
+                        "Invalid ID number! use `warns` to see all warnings!"
+                    )
+            else:
+                collection.delete_one({"_id": member.id})
+                embed.description = f"Unwarned {member.mention} from all warnings!"
+        else:
+            embed.description = f"No warnings found for {member.mention}!"
+        return embed
+
+    @commands.command(name="unwarn", aliases=["uwarn", "unwarns", "uwarns"])
+    @commands.has_permissions(manage_messages=True)
+    async def unwarn(self, ctx, member: discord.Member, index: int = None):
+        if index:
+            await ctx.send(embed=await self._unwarn(member, index - 1))
+        else:
+            await ctx.send(embed=await self._unwarn(member))
+
+    @app_commands.command(name="unwarn", description="Unwarn a member")
+    @app_commands.checks.has_permissions(manage_messages=True)
+    @app_commands.describe(member="The member to unwarn")
+    @app_commands.describe(index="The ID of the warn to remove")
+    async def unwarn_slash(
+        self,
+        interaction: discord.Interaction,
+        member: discord.Member,
+        index: int = None,
+    ):
+        if index:
+            await interaction.response.send_message(
+                embed=await self._unwarn(member, index - 1)
+            )
+        else:
+            await interaction.response.send_message(embed=await self._unwarn(member))
+
+    @unwarn_slash.error
+    async def unwarn_slash_error(self, interaction, error):
+        embed = discord.Embed(
+            title="Unwarn Failed",
+            colour=discord.Colour.red(),
+            timestamp=datetime.now(),
+        )
+        if isinstance(error, app_commands.errors.MissingPermissions):
+            embed.description = (
+                "You don't have the required permissions to unwarn members!"
+            )
+        elif isinstance(error, app_commands.errors.BotMissingPermissions):
+            embed.description = (
+                "I don't have the required permissions to unwarn members!"
+            )
+        else:
+            embed.description = "Unknown error!"
+            await report_error(
+                error_webhook_url=self.bot.unknown_error_webhook_url,
+                session=self.bot.web_client,
+                error=error,
+                content="Unwarn Slash Command Error",
+                author=interaction.user,
+                guild=interaction.guild,
+                channel=interaction.channel,
+                username="Unknown Error || BlankBot",
+            )
+        await interaction.response.send_message(embed=embed)
+
+    async def _link(self, guild_id, member, action):
+        db = self.bot.cluster["guilds"]
+        collection = db["link_perms"]
+        embed = discord.Embed(
+            colour=discord.Colour.green(),
+            timestamp=datetime.now(),
+        )
+        data = collection.find_one({"_id": guild_id})
+        if data:
+            users = data["users"]
+            if member.id in users and action:
+                embed.description = f"{member.mention} already has this permission!"
+            elif member.id not in users and action:
+                collection.update_one(
+                    {"_id": guild_id},
+                    {"$push": {"users": member.id}},
+                )
+                embed.description = (
+                    f"{member.mention} can send discord invite links now!"
+                )
+            elif member.id in users and not action:
+                collection.update_one(
+                    {"_id": guild_id},
+                    {"$pull": {"users": member.id}},
+                )
+                embed.description = (
+                    f"{member.mention} can't send discord invite links now!"
+                )
+            elif member.id not in users and not action:
+                embed.description = (
+                    f"{member.mention} already doesn't have this permission!"
+                )
+        else:
+            if action:
+                collection.insert_one({"_id": guild_id, "users": [member.id]})
+                embed.description = (
+                    f"{member.mention} can send discord invite links now!"
+                )
+            else:
+                embed.description = (
+                    f"{member.mention} already doesn't have this permission!"
+                )
+        return embed
+
+    @commands.command(name="link_perm", aliases=["link_perms"])
+    @commands.has_permissions(administrator=True)
+    async def link_perm(self, ctx, member: discord.Member, action: bool):
+        await ctx.send(embed=await self._link(ctx.guild.id, member, action))
+
+    @app_commands.command(
+        name="link_perm",
+        description="Set a member's ability to send discord invite links",
+    )
+    @app_commands.checks.has_permissions(administrator=True)
+    @app_commands.describe(member="The member to set the permission for")
+    @app_commands.describe(
+        action="Whether to allow or disallow the member to send discord invite links"
+    )
+    async def link_perm_slash(
+        self, interaction: discord.Interaction, member: discord.Member, action: bool
+    ):
+        await interaction.response.send_message(
+            embed=await self._link(interaction.guild_id, member, action)
+        )
+
+    @link_perm_slash.error
+    async def link_perm_slash_error(self, interaction, error):
+        embed = discord.Embed(
+            title="Link Permission Failed",
+            colour=discord.Colour.red(),
+            timestamp=datetime.now(),
+        )
+        if isinstance(error, app_commands.errors.MissingPermissions):
+            embed.description = (
+                "You don't have the required permissions to set link permissions!"
+            )
+        elif isinstance(error, app_commands.errors.BotMissingPermissions):
+            embed.description = (
+                "I don't have the required permissions to set link permissions!"
+            )
+        else:
+            embed.description = "Unknown error!"
+            await report_error(
+                error_webhook_url=self.bot.unknown_error_webhook_url,
+                session=self.bot.web_client,
+                error=error,
+                content="Link Permission Slash Command Error",
                 author=interaction.user,
                 guild=interaction.guild,
                 channel=interaction.channel,
