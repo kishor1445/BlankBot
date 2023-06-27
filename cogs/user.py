e181c366b86fe0228a6e9f60887783aae856dc3f
Kishor <kishorramanan5@gmail.com>
2023-06-27 10:54:23 +0530

    Create user.py

---
+++ cogs/user.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+
+class User(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="avatar", aliases=["av"])
+    async def avatar(self, ctx, user: discord.User = None):
+        user = user or ctx.author
+        await ctx.send(user.avatar.url)
+
+
+async def setup(bot):
+    await bot.add_cog(User(bot))
