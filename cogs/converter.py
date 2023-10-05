8110b7b485b31927c03dbb2b1adb795300476601
Kishor <kishorramanan5@gmail.com>
2023-10-05 23:57:21 +0530

    made the text reply to code block
    so it will be easy to copy

--- cogs/converter.py
+++ cogs/converter.py
@@ -129,48 +129,73 @@ class Converter(commands.Cog):
     @convert.command(name="ascii2text", aliases=["a2t"])
     async def ascii_to_text(self, ctx, *, ascii_numbers: str):
         await ctx.send(
-            "".join(chr(int(ascii_number)) for ascii_number in ascii_numbers.split())
+            "ASCII To Text: ```"
+            + "".join(chr(int(ascii_number)) for ascii_number in ascii_numbers.split())
+            + "```"
         )
 
     @convert.command(name="text2ascii", aliases=["t2a"])
     async def text_to_ascii(self, ctx, *, text: str):
-        await ctx.send(" ".join(str(ord(char)) for char in text))
+        await ctx.send(
+            "Text To ASCII: ```" + " ".join(str(ord(char)) for char in text) + "```"
+        )
 
     @convert.command(name="binary2text", aliases=["b2t"])
     async def binary_to_text(self, ctx, *, binary: str):
         await ctx.send(
-            "".join(chr(int(binary_number, 2)) for binary_number in binary.split())
+            "Binary To Text: ```"
+            + "".join(chr(int(binary_number, 2)) for binary_number in binary.split())
+            + "```"
         )
 
     @convert.command(name="text2binary", aliases=["t2b"])
     async def text_to_binary(self, ctx, *, text: str):
-        await ctx.send(" ".join(bin(ord(char))[2:] for char in text))
+        await ctx.send(
+            "Text To Binary: ```"
+            + " ".join(bin(ord(char))[2:] for char in text)
+            + "```"
+        )
 
     @convert.command(name="hex2text", aliases=["h2t"])
     async def hex_to_text(self, ctx, *, hex_: str):
-        await ctx.send("".join(chr(int(hex_number, 16)) for hex_number in hex_.split()))
+        await ctx.send(
+            "Hex to Text: ```"
+            + "".join(chr(int(hex_number, 16)) for hex_number in hex_.split())
+            + "```"
+        )
 
     @convert.command(name="text2hex", aliases=["t2h"])
     async def text_to_hex(self, ctx, *, text: str):
-        await ctx.send(" ".join(hex(ord(char))[2:] for char in text))
+        await ctx.send(
+            "Text To Hex: ```" + " ".join(hex(ord(char))[2:] for char in text) + "```"
+        )
 
     @convert.command(name="octal2text", aliases=["o2t"])
     async def octal_to_text(self, ctx, *, octal: str):
         await ctx.send(
-            "".join(chr(int(octal_number, 8)) for octal_number in octal.split())
+            "Octal To Text: ```"
+            + "".join(
+                chr(int(octal_number, 8)) for octal_number in octal.split() + "```"
+            )
         )
 
     @convert.command(name="text2octal", aliases=["t2o"])
     async def text_to_octal(self, ctx, *, text: str):
-        await ctx.send(" ".join(oct(ord(char))[2:] for char in text))
+        await ctx.send(
+            "Text To Octal: ```" + " ".join(oct(ord(char))[2:] for char in text) + "```"
+        )
 
     @convert.command(name="base64encode", aliases=["b64e"])
     async def base64_encode(self, ctx, *, text: str):
-        await ctx.send(base64.b64encode(text.encode()).decode())
+        await ctx.send(
+            "Base64 Encode: ```" + base64.b64encode(text.encode()).decode() + "```"
+        )
 
     @convert.command(name="base64decode", aliases=["b64d"])
     async def base64_decode(self, ctx, *, text: str):
-        await ctx.send(base64.b64decode(text.encode()).decode())
+        await ctx.send(
+            "Base64 Decode: ```" + base64.b64decode(text.encode()).decode() + "```"
+        )
 
     @convert.command(name="morse2text", aliases=["m2t"])
     async def morse_to_text(self, ctx, *, morse: str):
@@ -181,7 +206,7 @@ class Converter(commands.Cog):
                 msg += " "
             else:
                 msg += MORSE_TO_TEXT_DICT.get(char, char)
-        await ctx.send(msg)
+        await ctx.send("Morse To Text: ```" + msg + "```")
 
     @convert.command(name="text2morse", aliases=["t2m"])
     async def text_to_morse(self, ctx, *, text: str):
@@ -192,21 +217,21 @@ class Converter(commands.Cog):
                 msg += "/ "
             else:
                 msg += TEXT_TO_MORSE_DICT.get(char) + " "
-        await ctx.send(msg)
+        await ctx.send("Text To Morse: ```" + msg + "```")
 
     @convert.command(name="revtext", aliases=["rt", "revt"])
     async def text_to_reverse(self, ctx, *, text: str):
-        await ctx.send(text[::-1])
+        await ctx.send("Reversed Text: ```" + text[::-1] + "```")
 
     @convert.command(name="revword", aliases=["rw", "revw"])
     async def word_to_reverse(self, ctx, *, text: str):
-        await ctx.send(" ".join(text.split()[::-1]))
+        await ctx.send("Reversed Word: ```" + " ".join(text.split()[::-1]) + "```")
 
     @convert.command(name="shuffleWord", aliases=["sw", "shw"])
     async def word_to_shuffle(self, ctx, *, text: str):
         text = text.split()
         random.shuffle(text)
-        await ctx.send(" ".join(text))
+        await ctx.send("Shuffled Words: ```" + " ".join(text) + "```")
 
 
 async def setup(bot):
